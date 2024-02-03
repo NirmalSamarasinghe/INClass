09aaa9f22dd76018adee5a79bbd9d71af1f19f67
Nipun78 <nipunnirmal67@gmail.com>
2024-02-03 11:00:52 +0530

    done

---
+++ src/main/java/Customer.java
@@ -0,0 +1,18 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "customer",urlPatterns = "/customer")
+public class Customer extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getHeader("name");
+
+
+        System.out.println(name);
+    }
+}
